99b069676954bf5cd451c8cb7f1e10e229414e49
jonhayjr <jonhayjr@gmail.com>
2022-01-27 21:42:18 -0800

    Updated API Key variable

--- src/app/movie-service.service.ts
+++ src/app/movie-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +9,6 @@ export class MovieService {
   constructor(private http: HttpClient) { }
 
   getMovies(search: string) {
-    return this.http.get(`https://www.omdbapi.com/?apiKey=${environment.apiKey}&type=movie&s=${search}`)
+    return this.http.get(`https://www.omdbapi.com/?apiKey=${process.env.apiKey}&type=movie&s=${search}`)
   }
 }
