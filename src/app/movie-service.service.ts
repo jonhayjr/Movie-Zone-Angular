ad63d8d62146df014e36c4c11df99c00dd1a8f31
jonhayjr <jonhayjr@gmail.com>
2022-01-27 21:47:24 -0800

    Reverted .gitignore changes

--- src/app/movie-service.service.ts
+++ src/app/movie-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class MovieService {
   constructor(private http: HttpClient) { }
 
   getMovies(search: string) {
-    return this.http.get(`https://www.omdbapi.com/?apiKey=${process.env.apiKey}&type=movie&s=${search}`)
+    return this.http.get(`https://www.omdbapi.com/?apiKey=${environment.apiKey}&type=movie&s=${search}`)
   }
 }
